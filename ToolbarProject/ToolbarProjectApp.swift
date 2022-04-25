9694bbfd2351fc4f870f0ae7cb166a1f6998a369
Eingest <ak0805@live.de>
2022-04-25 18:36:46 +0200

    Initial Commit

---
+++ ToolbarProject/ToolbarProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarProjectApp.swift
+//  ToolbarProject
+//
+//  Created by Andreas Kiesel on 25.04.22.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
